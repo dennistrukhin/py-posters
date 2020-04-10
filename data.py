19b361f02552c226d673414402f16335ad081f9b
Denis Trukhin <dtrukhin@mercury.ru>
2020-04-10 18:11:36 +0300

    refactored a bit

---
+++ data.py
@@ -0,0 +1,81 @@
+sizes = [
+    {
+        "name": "30x30cm",
+        "points": 3543,
+    }, {
+        "name": "40x40cm",
+        "points": 4724,
+    }, {
+        "name": "50x50cm",
+        "points": 5907,
+    }, {
+        "name": "60x60cm",
+        "points": 7087,
+    }, {
+        "name": "100x100cm",
+        "points": 11811,
+    }, {
+        "name": "18x18in",
+        "points": 5400,
+    }, {
+        "name": "24x30in",
+        "points": 7200,
+    }, {
+        "name": "30x30in",
+        "points": 9000,
+    }
+]
+
+posters = {
+    "001": {
+        "description": "Office skyline",
+        "scheme": ['253239', '3E6352', '15BDC1', 'EDE6B0', 'D04E33'],
+        "substitutePalette": True,
+        "substituteSource": True,
+    },
+    "003": {
+        "description": "Tree",
+        "scheme": ['45826C', '8AD392', 'F2D068', 'F49A4C', 'E65549'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "004": {
+        "description": "Рябина",
+        "scheme": ['162F38', '427A55', '9EC135', 'D44A26', 'F6E87B'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "005": {
+        "description": "Киса сверху",
+        "scheme": ['D39A66', 'E4D988', 'C59566', 'AF484E', '442237'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "006": {
+        "description": "Дерево и забор",
+        "scheme": ['CCC9CF', '09335B', '204D81', 'E6E7D7', 'F3AC88'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "007": {
+        "description": "веточка в стакане",
+        "scheme": ['83A184', 'F0D7A6', 'EF8C2E', 'AB8F79', '6E2E2E'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "008": {
+        "description": "Цветы в офисе на подоконнике",
+        "scheme": ['406C4D', '7B8169', 'BDC1B9', 'D26B45', 'BDBFBF'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+}
+
+
+# 'BDBAAA', 'B9AA7A', 'BB803B', '976634', '252D1B',
+# '82C5A2', 'D79057', 'F0C582', 'FDEDB8', '1C2536',  # Мичуринка
+# '253239', '4D684F', 'C1AA64', 'EDE6B0', 'AAC5AA',
+# '253239', '3E6352', 'A1C576', 'EDE6B0', 'D04E33',
+# '15BDC1', '3E6352', '253239', 'EDE6B0', 'D04E33',
+
+# '532943', 'E0DFB6', '842F5C', 'BEC89A', 'D1C3B6',  # Цветы в офисе на подоконнике
--- main.py
+++ main.py
@@ -5,46 +5,34 @@ import utils
 import cv2
 from PIL import Image
 from math import pow, sqrt
+from data import posters
+
+KEY = "005"
+POSTER = posters[KEY]
 
 PIXEL_FACTOR = 1
-USE_AUTO_PALETTE = True
-SUBSTITUTE_PALETTE = True
-# SUBSTITUTE_PALETTE = False
+SUBSTITUTE_PALETTE = POSTER['substitutePalette']
 
-if USE_AUTO_PALETTE:
-    # image = cv2.imread("./input-3.jpg")
-    image = cv2.imread("./input/0404-04-1.jpg")
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (300, 300))
-
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
-
-    clt = KMeans(n_clusters=5)
-    clt.fit(image)
-
-    print(clt.cluster_centers_)
-    centroids = sorted(clt.cluster_centers_, key=itemgetter(0))
+# image = cv2.imread("./input-3.jpg")
+if POSTER['substituteSource']:
+    image = cv2.imread("./input/003.jpg")
 else:
-    centroids = utils.get_palette([10, 150, 180, 210, 255])
+    image = cv2.imread("./input/" + KEY + ".jpg")
 
-scheme = utils.scheme_from_hexes([
-    # 'BDBAAA', 'B9AA7A', 'BB803B', '976634', '252D1B',
-    # '82C5A2', 'D79057', 'F0C582', 'FDEDB8', '1C2536',  # Мичуринка
-    # '253239', '4D684F', 'C1AA64', 'EDE6B0', 'AAC5AA',
-    # '253239', '3E6352', 'A1C576', 'EDE6B0', 'D04E33',
-    # '253239', '3E6352', '15BDC1', 'EDE6B0', 'D04E33',  # Office BEST
-    # '15BDC1', '3E6352', '253239', 'EDE6B0', 'D04E33',
-    # '45826C', '8AD392', 'F2D068', 'F49A4C', 'E65549',  # Tree
-    # '162F38', '427A55', '9EC135', 'D44A26', 'F6E87B',  # Рябина
-    # 'D39A66', 'E4D988', 'C59566', 'AF484E', '442237',  # Киса сверху
-    # 'CCC9CF', '09335B', '204D81', 'E6E7D7', 'F3AC88',  # Дерево и забор
-    # '83A184', 'F0D7A6', 'EF8C2E', 'AB8F79', '6E2E2E',  # веточка в стакане
-    # '532943', 'E0DFB6', '842F5C', 'BEC89A', 'D1C3B6',  # Цветы в офисе на подоконнике
-    '406C4D', '7B8169', 'BDC1B9', 'D26B45', 'BDBFBF',  # Цветы в офисе на подоконнике
-])
-print(scheme)
 
-im = Image.open("./input/0404-04-1.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.resize(image, (300, 300))
+
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+clt = KMeans(n_clusters=5)
+clt.fit(image)
+
+centroids = sorted(clt.cluster_centers_, key=itemgetter(0))
+
+scheme = utils.scheme_from_hexes(POSTER['scheme'])
+
+im = Image.open("./input/" + KEY + ".jpg")
 (sx, sy) = im.size
 img = Image.new(im.mode, (sx * PIXEL_FACTOR, sy * PIXEL_FACTOR))
 pix = im.load()
@@ -72,5 +60,5 @@ for y in range(0, h):
                     )
 
 im.close()
-img.save("./output/0404-04-1.jpg", quality=100, dpi=(300, 300))
+img.save("./output/" + KEY + "-test.jpg", quality=100, dpi=(72, 72))
 img.close()
