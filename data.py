0f46fa79bf4b1efd91d3fbaf5750edbcaf601fa9
Denis Trukhin <dtrukhin@mercury.ru>
2020-04-11 16:02:52 +0300

    Borders + custom palettes

--- data.py
+++ data.py
@@ -1,29 +1,32 @@
-sizes = [
+dimensions = [
     {
         "name": "30x30cm",
-        "points": 3543,
-    }, {
-        "name": "40x40cm",
-        "points": 4724,
-    }, {
-        "name": "50x50cm",
-        "points": 5907,
-    }, {
-        "name": "60x60cm",
-        "points": 7087,
-    }, {
-        "name": "100x100cm",
-        "points": 11811,
-    }, {
-        "name": "18x18in",
-        "points": 5400,
-    }, {
-        "name": "24x30in",
-        "points": 7200,
-    }, {
-        "name": "30x30in",
-        "points": 9000,
-    }
+        # "points": 3543,
+        "points": 1000,
+        "border": 0.125,
+    },
+    # {
+    #     "name": "40x40cm",
+    #     "points": 4724,
+    # }, {
+    #     "name": "50x50cm",
+    #     "points": 5907,
+    # }, {
+    #     "name": "60x60cm",
+    #     "points": 7087,
+    # }, {
+    #     "name": "100x100cm",
+    #     "points": 11811,
+    # }, {
+    #     "name": "18x18in",
+    #     "points": 5400,
+    # }, {
+    #     "name": "24x30in",
+    #     "points": 7200,
+    # }, {
+    #     "name": "30x30in",
+    #     "points": 9000,
+    # }
 ]
 
 posters = {
@@ -33,17 +36,32 @@ posters = {
         "substitutePalette": True,
         "substituteSource": True,
     },
+    # "003": {
+    #     "description": "Tree",
+    #     "scheme": ['45826C', '8AD392', 'F2D068', 'F49A4C', 'E65549'],
+    #     "substitutePalette": True,
+    #     "substituteSource": False,
+    # },
     "003": {
         "description": "Tree",
-        "scheme": ['45826C', '8AD392', 'F2D068', 'F49A4C', 'E65549'],
+        "scheme": ['45826C', '8AD392', 'FFFFFF', 'FFFFFF', 'E65549'],
         "substitutePalette": True,
         "substituteSource": False,
     },
-    "004": {
+    "012": {
         "description": "Рябина",
-        "scheme": ['162F38', '427A55', '9EC135', 'D44A26', 'F6E87B'],
+        # "scheme": ['162F38', '427A55', '9EC135', 'D44A26', 'F6E87B'],
+        "scheme": ['162F38', 'FFFFFF', 'FFFFFF', 'D44A26', 'FFFFFF'],
         "substitutePalette": True,
         "substituteSource": False,
+        "usePredefinedCentroids": True,
+        "centroids": [
+            [45.15289256, 38.64582676, 14.76995142],
+            [80.4020495, 81.17536465, 35.67703814],
+            [123.99709546, 117.6972181,  80.20777125],
+            [146.00229286,  55.1601876,  34.60635748],
+            [170.6008436, 159.06149406, 134.17926518],
+        ],
     },
     "005": {
         "description": "Киса сверху",
@@ -57,14 +75,26 @@ posters = {
         "substitutePalette": True,
         "substituteSource": False,
     },
+    # "007": {
+    #     "description": "веточка в стакане",
+    #     "scheme": ['83A184', 'F0D7A6', 'EF8C2E', 'AB8F79', '6E2E2E'],
+    #     "substitutePalette": True,
+    #     "substituteSource": False,
+    # },
     "007": {
         "description": "веточка в стакане",
-        "scheme": ['83A184', 'F0D7A6', 'EF8C2E', 'AB8F79', '6E2E2E'],
+        "scheme": ['6E2E2E', 'F0D7A6', 'EF8C2E', '000000', '000000'],
         "substitutePalette": True,
         "substituteSource": False,
     },
     "008": {
         "description": "Цветы в офисе на подоконнике",
+        "scheme": ['406C4D', '7B8169', 'BDC1B9', 'D26B45', 'BDBFBF'],
+        "substitutePalette": True,
+        "substituteSource": False,
+    },
+    "009": {
+        "description": "Для тестов",
         "scheme": ['406C4D', '7B8169', 'BDC1B9', 'D26B45', 'BDBFBF'],
         "substitutePalette": True,
         "substituteSource": False,
--- main.py
+++ main.py
@@ -5,60 +5,64 @@ import utils
 import cv2
 from PIL import Image
 from math import pow, sqrt
-from data import posters
+from data import posters, dimensions
+from math import floor
 
-KEY = "005"
+KEY = "012"
 POSTER = posters[KEY]
 
 PIXEL_FACTOR = 1
 SUBSTITUTE_PALETTE = POSTER['substitutePalette']
 
-# image = cv2.imread("./input-3.jpg")
 if POSTER['substituteSource']:
     image = cv2.imread("./input/003.jpg")
 else:
     image = cv2.imread("./input/" + KEY + ".jpg")
 
+if image.shape[0] != image.shape[1]:
+    raise Exception("Input image should be square")
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image = cv2.resize(image, (300, 300))
+if POSTER['usePredefinedCentroids']:
+    centroids = POSTER['centroids']
+else:
+    exit()
+    paletteSourceImage = cv2.resize(image, (300, 300))
+    paletteSourceImage = paletteSourceImage.reshape((paletteSourceImage.shape[0] * paletteSourceImage.shape[1], 3))
 
-image = image.reshape((image.shape[0] * image.shape[1], 3))
+    clt = KMeans(n_clusters=5)
+    clt.fit(paletteSourceImage)
 
-clt = KMeans(n_clusters=5)
-clt.fit(image)
-
-centroids = sorted(clt.cluster_centers_, key=itemgetter(0))
+    centroids = sorted(clt.cluster_centers_, key=itemgetter(0))
 
 scheme = utils.scheme_from_hexes(POSTER['scheme'])
 
-im = Image.open("./input/" + KEY + ".jpg")
-(sx, sy) = im.size
-img = Image.new(im.mode, (sx * PIXEL_FACTOR, sy * PIXEL_FACTOR))
-pix = im.load()
-pix_new = img.load()
-(w, h) = im.size
+# for withBorder in [False, True]:
+for withBorder in [False]:
+    for dim in dimensions:
+        border_size = floor(dim['border'] * dim['points']) * (1 if withBorder else 0)
+        s = dim['points']
+        im = cv2.resize(image, (s - 2 * border_size, s - 2 * border_size))
+        img = Image.new(mode='RGB', size=(s, s), color=(0, 0, 0))
+        pix_new = img.load()
 
-for y in range(0, h):
-    print(y)
-    for x in range(0, w):
-        min_dist = 1000000
-        c_i = 0
-        (r, g, b) = pix[x, y]
-        for c in range(0, 5):
-            dist = sqrt(pow(centroids[c][0] - r, 2) + pow(centroids[c][1] - g, 2) + pow(centroids[c][2] - b, 2))
-            if dist < min_dist:
-                c_i = c
-                min_dist = dist
-        for dx in range(0, PIXEL_FACTOR):
-            for dy in range(0, PIXEL_FACTOR):
-                pix_new[x * PIXEL_FACTOR + dx, y * PIXEL_FACTOR + dy] = \
+        for y in range(0, s - 2 * border_size):
+            for x in range(0, s - 2 * border_size):
+                min_dist = 1000000
+                c_i = 0
+                (r, g, b) = im[y, x]
+                for c in range(0, 5):
+                    dist = sqrt(pow(centroids[c][0] - r, 2) + pow(centroids[c][1] - g, 2) + pow(centroids[c][2] - b, 2))
+                    if dist < min_dist:
+                        c_i = c
+                        min_dist = dist
+                pix_new[x + border_size, y + border_size] = \
                     (
                         scheme[c_i][0] if SUBSTITUTE_PALETTE else int(centroids[c_i][0]),
                         scheme[c_i][1] if SUBSTITUTE_PALETTE else int(centroids[c_i][1]),
                         scheme[c_i][2] if SUBSTITUTE_PALETTE else int(centroids[c_i][2])
                     )
 
-im.close()
-img.save("./output/" + KEY + "-test.jpg", quality=100, dpi=(72, 72))
-img.close()
+        img.save("./output/" + KEY + '-' + dim['name'] + '-' + ('border' if withBorder else '') + "-test.jpg",
+                 quality=100, dpi=(300, 300))
+        img.close()
